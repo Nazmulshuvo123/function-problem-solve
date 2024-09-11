d65ae107946545cee3b9e98aa47e1e05249a5769
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-11 22:18:10 +0600

    Take four parameters. Multiply the four numbers and then return the result

---
+++ task-1.js
@@ -0,0 +1,9 @@
+/**
+ * Take four parameters. Multiply the four numbers and then return the result
+ */
+
+function multiply(a, b, c, d){
+    let result = a * b * c * d;
+    return result; 
+}
+console.log(multiply(3, 4, 5, 6));
